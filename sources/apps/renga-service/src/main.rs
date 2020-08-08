c8d5958ee151d4d5c54cacc49e2d060502542c18
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-08 23:13:36 +0900

    test update

--- sources/apps/renga-service/src/main.rs
+++ sources/apps/renga-service/src/main.rs
@@ -2,11 +2,11 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Hello world!!!")
 }
 
 async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
+    HttpResponse::Ok().body("Hello world again!!!")
 }
 
 #[actix_rt::main]
