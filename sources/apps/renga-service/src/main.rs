572210818586118af51999c3dacd893dbfdbd036
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-09 00:24:25 +0900

    extractor sample

--- sources/apps/renga-service/src/main.rs
+++ sources/apps/renga-service/src/main.rs
@@ -1,6 +1,11 @@
-
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::get;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Greeting {
+    name: String,
+}
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!!!")
@@ -15,6 +20,16 @@ async fn index3() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+/*
+async fn hello(greeting: web::Path<Greeting>) -> impl Responder {
+    format!("Hello, {}!", greeting.name)
+}
+*/
+
+async fn hello(greeting: web::Path<Greeting>) -> actix_web::Result<String> {
+    Ok(format!("Hello, {}!", greeting.name))
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     println!("running...");
@@ -24,6 +39,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
             .service(index3)
+            .route("/hello/{name}", web::get().to(hello))
     })
         .bind("127.0.0.1:8088")?
         .run()
